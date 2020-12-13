b1e8562d92e489190f619b0afcb772ff14dd6582
Sejfi <r.seifert@centrum.cz>
2020-12-13 15:58:57 +0100

    Auto-generated commit

---
+++ assets/mocks/MockTilesConfig.ts
@@ -0,0 +1,19 @@
+import { TileConfig } from '../../app/models/tile-config';
+import { ReportType } from '../../app/enumerations/report-type.enum';
+import { TileChartType } from '../../app/enumerations/tile-chart.enum';
+import { DateRangeEnum } from '../../app/enumerations/date-range.enum';
+
+export const mockTilesConfig: TileConfig[] = [
+  {
+    index: 0,
+    report: ReportType.traffic,
+    chart: TileChartType.bar,
+    dateRange: { range: DateRangeEnum.Week, from: null, to: null }
+  },
+  {
+    index: 1,
+    report: ReportType.browser,
+    chart: TileChartType.pie,
+    dateRange: { range: DateRangeEnum.Week, from: null, to: null }
+  }
+];
